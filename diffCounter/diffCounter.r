913afcb277147636bae14c3cd791fe057db1d082
Kure <christian.parobek@gmail.com>
2016-04-25 14:25:59 -0400

    working version that accommodates biallelic and multiallelic vcfs

--- diffCounter/diffCounter.r
+++ diffCounter/diffCounter.r
@@ -22,43 +22,48 @@ library(stringr)
 ################ DEFINE FUNCTIONS #################
 ###################################################
 
-## Given a multiVCF, get the genotypes
-genoGet <- function(dataset) {
-  
+processer <- function(dataset) {
+
   ## REMOVE FIRST NINE COLUMNS FROM THE MULTIVCFs
   data <- dataset[-c(1:9)]
-  geno <- as.data.frame(sapply(data, function(x) str_extract(x, "[0123456789]")))
+  genos <- as.data.frame(sapply(data, function(x) str_extract(x, "[0123456789]")))
   
+  mat <- as.matrix(genos)
+  obj <- NULL
+  
+  # Count pairwise differences
+  # Alleles are coded as 0,1,2, or 3 depending on the nucleotide.
+  # So find the differences first, but then convert to as.logical to just a T or F answer
+  # Then convert back to numeric to turn all the trues into 1s
+  # So essentially this just takes any number > 0 and turns it into a 1
+  # Then sum that to identify the total number of pairwise differences.
+  for (i in 1:ncol(mat)) {
+    for (j in 1:ncol(mat)) {
+      obj <- append(obj, sum(as.numeric(as.logical(as.numeric(mat[,i]) - as.numeric(mat[,j]))), na.rm = TRUE))
+    } 
+  }
+  
+  res <- matrix(obj, nrow = ncol(mat), ncol = ncol(mat)) # make a matrix out of it
+  
+  rownames(res) <- colnames(genos)
+  colnames(res) <- colnames(genos)
+  
+  return(res)
 }
-
 
 ###################################################
 ################## READ IN DATA ###################
 ###################################################
 
-## READ IN THE Pf AND Pv MULTIVCFs
-pf <- read.table("esbl_UG.vcf", comment.char="#", header=TRUE)
+## READ IN THE MULTIVCF
 
-genos <- genoGet(pf)
-mat <- as.matrix(genos)
-class(mat)
-genos$ESBL01 - genos$ESBL02
+st131 <- read.table("st131.biallelic.vcf", comment.char="#", header=TRUE)
+nonst131 <- read.table("nonst131.biallelic.vcf", comment.char="#", header=TRUE)
 
-obj <- NULL
+results_st131 <- processer(st131)
+results_nonst131 <- processer(nonst131)
 
-# Count pairwise differences
-# Alleles are coded as 0,1,2, or 3 depending on the nucleotide.
-# So find the differences first, but then convert to as.logical to just a T or F answer
-# Then convert back to numeric to turn all the trues into 1s
-# So essentially this just takes any number > 0 and turns it into a 1
-# Then sum that to identify the total number of pairwise differences.
-for (i in 1:ncol(mat)) {
-  for (j in 1:ncol(mat)) {
-    obj <- append(obj, sum(as.numeric(as.logical(as.numeric(mat[,i]) - as.numeric(mat[,j]))), na.rm = TRUE))
-  } 
-}
+write.table(results_st131, "results_biallelic_st131.txt", sep="\t")
+write.table(results_nonst131, "results_biallelic_nonst131.txt", sep="\t")
 
-res <- matrix(obj, nrow = ncol(mat), ncol = ncol(mat)) # make a matrix out of it
-
-obj[order(obj)] # find lowest numer of SNPs
 
